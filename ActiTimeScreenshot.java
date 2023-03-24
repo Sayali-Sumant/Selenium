badf6d2dcf0936a4203eddb3893a6feed43aab3e
Sayali-Sumant <128784580+Sayali-Sumant@users.noreply.github.com>
2023-03-24 13:39:31 +0530

    Add files via upload

---
+++ ActiTimeScreenshot.java
@@ -0,0 +1,44 @@
+package DEmo;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ActiTimeScreenshot {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+		String timestamp = time.toString().replace(':','-');
+		
+		ChromeOptions op=new ChromeOptions();
+		op.addArguments("--remote-allow-origins=*");	
+		WebDriver wd=new ChromeDriver(op);
+		
+		
+		TakesScreenshot ts=(TakesScreenshot) wd;
+		
+		wd.get("http://demo.actitime.com/login.do");
+		
+		File destfile = ts.getScreenshotAs(OutputType.FILE);
+
+           
+        File root_file=new File("./images/actitime1"+timestamp+".png");
+      
+        FileUtils.copyFile( destfile,root_file);
+
+	}
+	
+
+
+	}
+
+
